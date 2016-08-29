35457c2b89df14673214b9722a71dc55fd9d7e7a
VL <vl16041604@gmail.com>
2016-08-29 22:55:07 +0300

    add local L, R for j

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
-import java.util.*;
+import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Main
 {
@@ -29,17 +30,22 @@ public class Main
 	private static void solveArray(ArrayList<Integer> a)
 	{
 		int size = a.size();
+		int jl, jr;
 
 		for (int j = 0; j < size; j += 3)
+		{
+			jl = a.get(j);
+			jr = a.get(j + 1);
 			for (int i = 0; i < size; i += 3)
 			{
 				if (j == i)
 					continue;
 
-				if (a.get(j) < a.get(i)
-					&& a.get(j + 1) > a.get(i + 1))
+				if (jl < a.get(i)
+					&& jr > a.get(i + 1))
 					a.set(j + 2, a.get(j + 2) + 1);
 			}
+		}
 	}
 
 	private static void printArray(ArrayList<Integer> a)
