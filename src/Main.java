aa6c6d6cca8df5210be0c23babf8d163debee5ec
VL <vl16041604@gmail.com>
2016-09-05 09:17:14 +0300

    arrays replaced with segments, added sort, unsort

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class Main
 {
-	private static ArrayList<Integer> array; 
+	private static ArrayList<Segment> segments; 
 
 	public static void main(String[] args)
 	{
@@ -13,55 +13,36 @@ public class Main
 	private static void runDNested(Scanner input)
 	{
 		int size = input.nextInt();
-		array = new ArrayList<Integer>(3 * size);
+		segments = new ArrayList<Segment>(size);
 
 		for (int i = 0; i < size; i++)
 		{
-			array.add(input.nextInt());
-			array.add(input.nextInt());
-			array.add(0);
+			segments.add(new Segment(
+							 input.nextInt(),
+							 input.nextInt(), 0, i));
 		}
 
-		solveArray(array);
-		printArray(array);
+		solveArray(segments);
+		printArray(segments);
 	}
 
-	private static void solveArray(ArrayList<Integer> a)
+	private static void solveArray(ArrayList<Segment> a)
 	{
 		int size = a.size();
 		int jl, jr, il, ir;
-		int lmin = a.get(0), rmax = a.get(1);
+		int lmin = a.get(0).getL(), rmax = a.get(0).getR();
 
+		sort(a);
 
-		ArrayList<ArrayList<Integer>> list = new ArrayList<ArrayList<Integer>>();
-		for (int j = 0; j < size; j += 3)
+		for (int j = 0; j < size; j++)
 		{
-			ArrayList<Integer> item = new ArrayList<Integer>();
-			item.add(a.get(j));
-			item.add(a.get(j + 1));
-			item.add(a.get(j + 2));
+			jl = a.get(j).getL();
+			jr = a.get(j).getR();
 
-			list.add(item);
-		}
-		Collections.sort(list, 
-			new Comparator<ArrayList>(){
-				public int compare(ArrayList<Integer> obj1, 
-								   ArrayList<Integer> obj2)
-				{
-					return  obj1.get(0) - obj2.get(0);
-				}
-			});
-
-
-		for (int j = 0; j < size; j += 3)
-		{
-			jl = a.get(j);
-			jr = a.get(j + 1);
-
-			for (int i = j + 3; i < size; i += 3)
+			for (int i = j + 1; i < size; i++)
 			{
-				il = a.get(i);
-				ir = a.get(i + 1);
+				il = a.get(i).getL();
+				ir = a.get(i).getR();
 
 				if (j == 0)
 				{
@@ -74,69 +55,101 @@ public class Main
 				if (jl < il)
 				{
 					if (jr > ir)
-						a.set(j + 2, a.get(j + 2) + 1);
+						a.get(j).setNum(a.get(j).getNum() + 1);
 				}
 				else
 				if (ir > jr)
-					a.set(i + 2, a.get(i + 2) + 1);
+					a.get(i).setNum(a.get(i).getNum() + 1);
 			}
 		}
 	}
 
-	private static void printArray(ArrayList<Integer> a)
+	private static void sort(ArrayList<Segment> a)
+	{
+		Collections.sort(a, 
+			new Comparator<Segment>(){
+				public int compare(Segment obj1, 
+								   Segment obj2)
+				{
+					return  obj1.getL() - obj2.getL();
+				}
+			});
+	}
+
+	private static void unsort(ArrayList<Segment> a)
+	{
+		Collections.sort(a, 
+			new Comparator<Segment>(){
+				public int compare(Segment obj1, 
+								   Segment obj2)
+				{
+					return  obj1.getPos() - obj2.getPos();
+				}
+			});
+	}
+
+	private static void printArray(ArrayList<Segment> a)
 	{
 		StringBuilder s = new StringBuilder();
 
-		for (int j = 0; j < a.size(); j += 3)
-			s.append(a.get(j + 2)).append('\n');
+		unsort(a);
+
+		for (int j = 0; j < a.size(); j++)
+			s.append(a.get(j).getNum()).append('\n');
 
 		System.out.print(s);
 	}
+};
 
-	private class Segment
+class Segment
+{
+	int l,r,num,pos;
+
+	public Segment(int l, int r, int num, int pos)
 	{
-		int l,r,num,pos;
+		this.l = l;
+		this.r = r;
+		this.num = num;
+		this.pos = pos;
+	}
 
+	public void setL(int l)
+	{
+		this.l = l;
+	}
 
-		public void setL(int l)
-		{
-			this.l = l;
-		}
+	public int getL()
+	{
+		return l;
+	}
 
-		public int getL()
-		{
-			return l;
-		}
+	public void setR(int r)
+	{
+		this.r = r;
+	}
 
-		public void setR(int r)
-		{
-			this.r = r;
-		}
+	public int getR()
+	{
+		return r;
+	}
 
-		public int getR()
-		{
-			return r;
-		}
+	public void setNum(int num)
+	{
+		this.num = num;
+	}
 
-		public void setNum(int num)
-		{
-			this.num = num;
-		}
+	public int getNum()
+	{
+		return num;
+	}
 
-		public int getNum()
-		{
-			return num;
-		}
+	public void setPos(int pos)
+	{
+		this.pos = pos;
+	}
 
-		public void setPos(int pos)
-		{
-			this.pos = pos;
-		}
-
-		public int getPos()
-		{
-			return pos;
-		}}
+	public int getPos()
+	{
+		return pos;
+	}
 }
-
-
